3481522eb607a333e50ecdecf166edc1fd07e846
FableJames <james.hu@fable.com.tw>
2023-12-27 16:56:56 +0800

    add missing file

---
+++ ecpay_invoice_tw/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from . import sdk
+from . import models
+from . import wizard
+from . import report
+from . import controllers
---
+++ ecpay_invoice_tw/__manifest__.py
@@ -0,0 +1,41 @@
+{
+    'name': 'ECPay 綠界第三方電子發票模組',
+    'version': '16.0.1.0',
+    'category': 'Accounting',
+    'author': 'ECPAY',
+    'website': 'http://www.ecpay.com.tw',
+    'description': """
+        綠界 台灣電子發票Odoo模組\n
+        使用 pyhton3\n
+        需要依賴模組 ecpay_invoice3\n
+        sudo pip3 install ecpay_invoice3
+    """,
+    'summary': '電子發票 (Invoice): ECPay 綠界第三方電子發票模組',
+    'depends': ['base','account', 'sale', 'stock'],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/ecpay_invoice_groups.xml',
+        'views/res_config_setting_view.xml',
+        'views/account_invoice_view.xml',
+        'views/uniform_invoice_view.xml',
+        'views/sale_order_view.xml',
+        'views/res_company.xml',
+        'views/res_partner_views.xml',
+        'wizard/invoice_invalid_wizard.xml',
+        'wizard/action_invalid_invoice_wizard.xml',
+        'views/menu.xml',
+        'data/demo.xml',
+        'report/uniform_invoice_report.xml'
+    ],
+    'external_dependencies': {
+        'python': ['pycryptodome'],
+        'bin': [],
+    },
+    "assets": {
+        "web.assets_frontend": [
+            "ecpay_invoice_tw/static/src/css/invoice.css",
+        ],
+    },
+    'installable': True,
+    'application': True,
+}
